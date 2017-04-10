8a5a98ba428ce3539d0483bfbf3f53c610c8bdb7
Ladislav Sopko <lsopko@3di.it>
2017-04-10 09:22:06 +0200

    dev in progress

--- Test/Program.cs
+++ Test/Program.cs
@@ -17,7 +17,7 @@ namespace Test
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			//Application.Run(new States.document.DocumentStateTestForm());
-			Application.Run(new Form4());
+			Application.Run(new Form8());
 		}
 	}
 }
